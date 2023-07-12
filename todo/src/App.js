2259c261e92cd43187009cc27aa881f0541bd0c6
Anasta2002 <93197598+Anasta2002@users.noreply.github.com>
2023-07-12 11:18:53 +0200

    comments

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import AddTaskForm from './components/AddTaskForm/AddTaskForm';
 
 function App() {
-  const [tasks, setTask] = useState([])
-  const add_task = new_task=> setTask([...tasks, new_task]);
-  const remove_task = id => setTask(tasks.filter((pr => pr.id !== id)))
 
+  const [tasks, setTask] = useState([])
+
+  const add_task = new_task=> setTask([...tasks, new_task])
+  
+  const remove_task = id => {
+    //здесь будет функция по удаления одной задачи, возможно типа такого: setTask(tasks.filter((pr => pr.id !== id)))
+  }
+  
   return (
     <div className="App">
         <AddTaskForm add_task={add_task} remove_task={remove_task} />
