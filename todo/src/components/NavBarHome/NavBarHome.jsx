bd7ef403d26ddf05f38cb401d10eecdde0264ed2
Arthur <abasov.ne@gmail.com>
2023-07-20 19:38:18 +0200

    hiden_&_unHiden_nawBar_Btn

--- todo/src/components/NavBarHome/NavBarHome.jsx
+++ todo/src/components/NavBarHome/NavBarHome.jsx
@@ -7,6 +7,7 @@ import TasksContainer from '../TasksContainer';
 import { Link } from 'react-router-dom'
 import Select from 'react-select'
 import { dayOptions } from '../../data/data_select';
+import { CiCircleList } from 'react-icons/ci'
 
 
 
@@ -29,86 +30,103 @@ export default function NavBarHome() {
 
 
 
-//создается объект, у которого ключами являются дни недели, внутри которых пустой массив, в которые мы будем пушить наши таски, чтобы они отображались отдельно в каждом дне
-const initialTasks = {
-    Monday: [{id: 1, value: 'Sleep all day'}, {id: 2, value: 'Play games'}],
-    Tuesday: [],
-    Wednesday: [],
-    Thursday: [],
-    Friday: [],
-    Saturday: [],
-    Sunday: []
-}
+    //создается объект, у которого ключами являются дни недели, внутри которых пустой массив, в которые мы будем пушить наши таски, чтобы они отображались отдельно в каждом дне
+    const initialTasks = {
+
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: [],
+        Saturday: [],
+        Sunday: []
+    }
 
 
 
-const [tasks, setTasks] = useState(initialTasks)
-const [selectedDay, setSelectedDay] = useState('Monday')
+    const [tasks, setTasks] = useState(initialTasks)
+    const [selectedDay, setSelectedDay] = useState('Monday')
+    const [isHidden, setIsHidden] = useState(false)
 
-console.log(tasks);
-
-
-
-// setTask обновляет tasks
-const add_task = (task) => {
-    setTasks(tasks => ({
-        ...tasks, 
-        [selectedDay]: [...tasks[selectedDay], task] //обновляем массив у выбранного дня и добавляем таск
-    }));
-}
-
-const newTasks = Object.keys(tasks) //создание массива из объекта
+    console.log(tasks);
 
 
 
-const colorStylesSelect = {
-    control: (styles) => ({ ...styles, backgroundColor: "#212121", height: '63px', border: 'none', color: 'f2f2f2' }),
-    option: (styles) => ({ ...styles })
-};
+    // setTask обновляет tasks
+    const add_task = (task) => {
+        setTasks(tasks => ({
+            ...tasks,
+            [selectedDay]: [...tasks[selectedDay], task] //обновляем массив у выбранного дня и добавляем таск
+        }));
+    }
+
+    const newTasks = Object.keys(tasks) //создание массива из объекта
 
 
-const handleChangeSelect = (selectedOption) => {
-    setSelectedDay(selectedOption.value)
-};
+
+    const colorStylesSelect = {
+        control: (styles) => ({ ...styles, backgroundColor: "#212121", height: '63px', border: 'none', color: 'f2f2f2' }),
+        option: (styles) => ({ ...styles })
+    };
 
 
+    const handleChangeSelect = (selectedOption) => {
+        setSelectedDay(selectedOption.value)
+    };
 
-return (
-    <div className={s.main}>
-        <div className={s.navbar}>
-            <h1 className={s.navbar_title}>Todos</h1>
-            
-            <form onSubmit={submit} className={s.form}>
-                <div className={s.item_container}>
-                    <AddTaskIcon />
-                    <input type='text' name='task' className={s.textarea} placeholder='Add task' />
-                </div>
+    const toggleHidden = () => {
+        setIsHidden(prevState => !prevState)
+    };
 
-                <div className={s.item_container}>
-                    {/* <SelectDay /> */}
-                    <Select
-                        options={dayOptions}
-                        onChange={handleChangeSelect}
-                        styles={colorStylesSelect}
-                        name='day'
-                        className={s.selector}
-                    />
-                </div>
-                
-                <button className={s.button}>
-                    <GiClick style={{ fill: '#b05fff' }} />  &nbsp;
-                    Click here to add task
-                </button>
+    const hendleActivMenu = () => {
+        setIsHidden(false)
+    };
 
-                <Link to={'/calendar'} className={s.redirect_calendar}>
-                    <BsFillCalendarDayFill style={{ fill: '#b05fff' }} /> &nbsp;
-                    Go to Calendar
-                </Link>
-            </form>
+    const hiddenNav = [s.navbar, isHidden ? s.navbarHiden : ''].join(' ');
+
+    return (
+        <div className={s.main}>
+            <div className={`${s.navbar} ${hiddenNav}`} >
+
+                <CiCircleList 
+                    onClick={toggleHidden}
+                    className={s.onHiddenBtn}
+                />
+
+                <h1 className={s.navbar_title}>Todos</h1>
+
+                <form onSubmit={submit} className={s.form}>
+                    <div className={s.item_container}>
+                        <AddTaskIcon />
+                        <input type='text' name='task' className={s.textarea} placeholder='Add task' />
+                    </div>
+
+                    <div className={s.item_container}>
+                        {/* <SelectDay /> */}
+                        <Select
+                            options={dayOptions}
+                            onChange={handleChangeSelect}
+                            styles={colorStylesSelect}
+                            name='day'
+                            className={s.selector}
+                        />
+                    </div>
+
+                    <button className={s.button}>
+                        <GiClick style={{ fill: '#b05fff' }} />  &nbsp;
+                        Click here to add task
+                    </button>
+
+                    <Link to={'/calendar'} className={s.redirect_calendar}>
+                        <BsFillCalendarDayFill style={{ fill: '#b05fff' }} /> &nbsp;
+                        Go to Calendar
+                    </Link>
+                </form>
+            </div>
+            <div>
+                <CiCircleList onClick={hendleActivMenu} />
+                <TasksContainer newTasks={newTasks} tasks={tasks} />
+            </div>
+
         </div>
-        <div>
-            <TasksContainer newTasks={newTasks} tasks={tasks} />
-        </div>
-    </div>
-)
+    )
 }
