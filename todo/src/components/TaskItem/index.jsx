6a1eccd478a0a0ee28092e133c16b8f4edb41fcc
Olha Sotova <sotovaola21@gmail.com>
2023-07-20 12:51:30 +0200

    delete_function and cross_task function

--- todo/src/components/TaskItem/index.jsx
+++ todo/src/components/TaskItem/index.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
 import s from './index.module.css'
 
-export default function TaskItem({task}) {
+export default function TaskItem({ task, delete_task, id }) {
 
-  const [checked, setChecked] = useState(false)
+  const [ischecked, setIsChecked] = useState(false)
 
-  
+
 
   return (
     <div className={s.task}>
-      <label>
-        <input type="checkbox" checked={checked} onChange={() => setChecked(!checked)} className={s.task_input} />
-        {task}
-        </label>
+      <label className={s.custom_checkbox} >
+        <input type="checkbox" checked={ischecked} onChange={() => setIsChecked(!ischecked)} className={s.task_input} />
+        <span className={s.input_span}></span>
+        <span className={ ischecked ? s.task_completed : '' } onClick={() => { delete_task(id) }} >
+          {task}
+        </span>
+
+      </label>
     </div>
 
   )
--- todo/src/pages/Home.jsx
+++ todo/src/pages/Home.jsx
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NavBarHome from '../components/NavBarHome/NavBarHome'
 
 export default function Home() {
-    const [tasks, setTask] = useState([])
-    const add_task = new_task => setTask([...tasks, new_task])
 
     return (
         <div>
-            <NavBarHome add_task={add_task} />
+            <NavBarHome  />
         </div>
     )
 }
