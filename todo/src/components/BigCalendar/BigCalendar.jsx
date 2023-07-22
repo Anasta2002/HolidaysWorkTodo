d001785bbb8dc2c84138f8ddeb3b018abce5344d
Anasta2002 <93197598+Anasta2002@users.noreply.github.com>
2023-07-22 17:20:42 +0200

    part of calendar

--- todo/src/components/BigCalendar/BigCalendar.jsx
+++ todo/src/components/BigCalendar/BigCalendar.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import React, { useState } from 'react'
+import { Calendar as BigCalendar, momentLocalizer, CalendarProps } from 'react-big-calendar'
 import moment from 'moment'
+import s from './index.module.css'
+import Calendar from 'react-production-calendar'
 
 const localizer = momentLocalizer(moment)
 
+export default function PlanCalendar() {
+    const [calendarValue, setCalendarValue] = useState(new Date())
 
-export default function BigCalendar() {
+    const [ year, setYear ] = useState(2023);
+    const [ month, setMonth ] = useState( (new Date()).getMonth() + 1 );
+
     return (
-        <div style={{height: '900px', width: '1000px'}}>
-            <Calendar
-              localizer={localizer}
-              style={{ height: '500px' }}
-            />
+        <div style={{height: '100%'}}>
+            {/* <BigCalendar
+                localizer={localizer}
+                // events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+                className={s.calendar_container}
+                // onChange={setCalendarValue}
+                // value={calendarValue}
+            /> */}
+            {/* <Calendar
+                visibleYear = { false }
+                year = { 2023 }
+                month = { month }
+            /> */}
         </div>
     )
 }
